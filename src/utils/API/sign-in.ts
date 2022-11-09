fbfa705180a5e6f437bbb8769ea62b033bf06e20
dab10 <dab10@list.ru>
2022-11-10 01:30:33 +0300

    feat: add auth API function and eslint

---
+++ src/utils/API/sign-in.ts
@@ -0,0 +1,37 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { host } from '../const/const';
+
+type User = {
+  login: string;
+  password: string;
+};
+
+export const signIn = createAsyncThunk('data/signIn', async (user: User, { rejectWithValue }) => {
+  try {
+    const response = await fetch(`${host}/auth/signin`, {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw new Error(`Error! Status: ${result.statusCode}. Message: ${result.message}`);
+    }
+    localStorage.setItem('token', JSON.stringify(result.token));
+
+    return result;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log('Error message: ', error.message);
+      return rejectWithValue(error.message);
+    } else {
+      console.log('Unexpected error: ', error);
+      return rejectWithValue('An unexpected error occurred');
+    }
+  }
+});
---
+++ src/utils/API/sign-up.ts
@@ -0,0 +1,37 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { host } from '../const/const';
+
+type User = {
+  name: string;
+  login: string;
+  password: string;
+};
+
+export const signUp = createAsyncThunk('data/signUp', async (user: User, { rejectWithValue }) => {
+  try {
+    const response = await fetch(`${host}/auth/signup`, {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw new Error(`Error! Status: ${result.statusCode}. Message: ${result.message}`);
+    }
+
+    return result;
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log('Error message: ', error.message);
+      return rejectWithValue(error.message);
+    } else {
+      console.log('Unexpected error: ', error);
+      return rejectWithValue('An unexpected error occurred');
+    }
+  }
+});
