9952330f5bb181276952b55cc1fda1d1f60852aa
yurilabatsevich <yurilabatsevich@gmail.com>
2022-11-17 00:30:21 +0300

    refactor: add browserrouter instead hashrowter

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { ROUTES } from 'utils/const/routes';
-import { Routes, Route, Navigate, HashRouter } from 'react-router-dom';
+import { Routes, Route, Navigate, HashRouter, Router, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Page404 from 'pages/Page404/Page404';
 import SignUp from 'pages/SignUp/SignUp';
@@ -14,7 +14,7 @@ import Welcome from 'pages/Welcome/Welcome';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path={ROUTES.HOME_PAGE} element={<Layout />}>
           <Route index element={<Home />} />
@@ -28,7 +28,7 @@ function App() {
           <Route path={ROUTES.ANY_PAGE} element={<Navigate to={ROUTES.NOT_FOUND_PAGE} replace />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
