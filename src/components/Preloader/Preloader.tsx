9ff3be93cadbd83b0370dc566e746b12634c77ee
yurilabatsevich <yurilabatsevich@gmail.com>
2022-11-17 22:18:11 +0300

    refactor(preloader): fix styles

--- src/components/Preloader/Preloader.tsx
+++ src/components/Preloader/Preloader.tsx
@@ -1,9 +1,10 @@
 import { Space, Spin } from 'antd';
 import React from 'react';
+import './preloader.scss';
 
 export const Preloader: React.FC = () => {
   return (
-    <Space size="middle">
+    <Space size="middle" className="preloader">
       <Spin size="default" />
     </Space>
   );
