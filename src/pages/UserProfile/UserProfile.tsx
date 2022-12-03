391159e826351e7a740782831466dcc9f3748756
Elem <elem15@yandex.ru>
2022-12-03 22:29:03 +0300

    feat: reset form after language toggle

--- src/pages/UserProfile/UserProfile.tsx
+++ src/pages/UserProfile/UserProfile.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './UserProfile.scss';
 import { Button, Form, Input, Row, Typography } from 'antd';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
@@ -21,6 +21,11 @@ const UserProfile: React.FC = () => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const { name, login, token, status } = useAppSelector((state) => state.auth);
+  const [nameRequired, setNameRequired] = useState(`${t('formRules.nameRequired')}`);
+  useEffect(() => {
+    setNameRequired(`${t('formRules.nameRequired')}`);
+    console.log(nameRequired);
+  }, [setNameRequired, t, t('formRules.nameRequired')]);
   const success = () => {
     navigate(ROUTES.HOME_PAGE);
   };
@@ -35,67 +40,73 @@ const UserProfile: React.FC = () => {
     };
     !token && deleteSuccess();
   }, [login, name, navigate, t, token]);
+  const RenderForm = ({ nameRequired }: { nameRequired: string }) => {
+    return (
+      <Form name="basic" onFinish={onFinish} autoComplete="off">
+        <Typography.Title level={2}>{t('sign.editProfile')}</Typography.Title>
+        <Form.Item
+          label={t('sign.name')}
+          name="name"
+          rules={[
+            { required: true, message: nameRequired },
+            { type: 'string', min: 3, message: `${t('formRules.nameLength')}` },
+          ]}
+        >
+          <Input placeholder={name} />
+        </Form.Item>
+        <Form.Item
+          label={t('sign.login')}
+          name="login"
+          rules={[
+            { required: true, message: `${t('formRules.loginRequired')}` },
+            { type: 'string', min: 3, message: `${t('formRules.loginLength')}` },
+          ]}
+        >
+          <Input placeholder={login} />
+        </Form.Item>
+        <Form.Item
+          label={t('sign.password')}
+          name="password"
+          rules={[
+            { required: true, message: `${t('formRules.passwordRequired')}` },
+            { type: 'string', min: 8, message: `${t('formRules.passwordLength')}` },
+            {
+              message: `${t('formRules.passwordPattern')}`,
+              pattern: /^[A-Za-z0-9_]+$/,
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Row justify="center">
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              {t('sign.submit')}
+            </Button>
+          </Form.Item>
+        </Row>
+        <br />
+        <br />
+        <Row justify="center">
+          <Button
+            type="primary"
+            danger
+            onClick={(e) =>
+              showDeleteConfirm(e, dispatch, 'user', `${t('message.account')}`, '', t)
+            }
+          >
+            {t('sign.delete')}
+          </Button>
+        </Row>
+      </Form>
+    );
+  };
+
   return (
     <main>
       {status === LOADING && <Preloader />}
       <Row justify="center">
-        <Form name="basic" onFinish={onFinish} autoComplete="off">
-          <Typography.Title level={2}>{t('sign.editProfile')}</Typography.Title>
-          <Form.Item
-            label={t('sign.name')}
-            name="name"
-            rules={[
-              { required: true, message: `${t('formRules.nameRequired')}` },
-              { type: 'string', min: 3, message: `${t('formRules.nameLength')}` },
-            ]}
-          >
-            <Input placeholder={name} />
-          </Form.Item>
-          <Form.Item
-            label={t('sign.login')}
-            name="login"
-            rules={[
-              { required: true, message: `${t('formRules.loginRequired')}` },
-              { type: 'string', min: 3, message: `${t('formRules.loginLength')}` },
-            ]}
-          >
-            <Input placeholder={login} />
-          </Form.Item>
-          <Form.Item
-            label={t('sign.password')}
-            name="password"
-            rules={[
-              { required: true, message: `${t('formRules.passwordRequired')}` },
-              { type: 'string', min: 8, message: `${t('formRules.passwordLength')}` },
-              {
-                message: `${t('formRules.passwordPattern')}`,
-                pattern: /^[A-Za-z0-9_]+$/,
-              },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
-          <Row justify="center">
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                {t('sign.submit')}
-              </Button>
-            </Form.Item>
-          </Row>
-          <br />
-          <br />
-          <Row justify="center">
-            <Button
-              type="primary"
-              danger
-              onClick={(e) =>
-                showDeleteConfirm(e, dispatch, 'user', `${t('message.account')}`, '', t)
-              }
-            >
-              {t('sign.delete')}
-            </Button>
-          </Row>
-        </Form>
+        <RenderForm nameRequired={nameRequired} />
       </Row>
     </main>
   );
